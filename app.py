3eeb8647627a5882956690a4516d658b744e2050
residentaze <gasimlianar622@gmail.ru>
2024-05-02 11:10:26 +0400

    Flask_project

--- app.py
+++ app.py
@@ -1,51 +1,99 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///litebase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Post(db.Model):
+
+
+class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
-@app.route("/index")
-@app.route("/")
+@app.route('/')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/posts")
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/posts')
 def posts():
-    posts = Post.query.all()
-    return render_template('posts.html', posts=posts)
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+@app.route('/posts/<int:id>/del')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "DELETE ERROR"
 
 
-@app.route("/create", methods=['POST', 'GET'])
-def create():
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
     if request.method == 'POST':
-        title = request.form['title']
-        text = request.form['text']
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
 
-        post = Post(title=title, text=text)
 
         try:
-            db.session.add(post)
             db.session.commit()
-            return redirect('/create')
+            return redirect('/posts')
         except:
-            return 'При добавлении возникла ощибка!'
+            return "EDIT ERROR"
     else:
-        return render_template('create.html')
+        return render_template('post_update.html', article=article)
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route('/create_article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "ADDITION ERROR"
+    else:
+        return render_template('create_article.html')
 
 
-if __name__ == '__main__':
+
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
